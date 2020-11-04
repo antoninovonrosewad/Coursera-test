721e11e9fd084e91b9bf227117197bfdb028d9b2
Antonino S. Fabillar <63834111+antoninovonrosewad@users.noreply.github.com>
2020-11-04 10:36:24 +0800

    Update script.js

--- Module5-Solution/Sulotion/js/script.js
+++ Module5-Solution/Sulotion/js/script.js
@@ -97,8 +97,8 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-        // document.querySelector.("#main-content");
-        // .innerHTML = homeHtml;
+        document.querySelector.("#main-content");
+        .innerHTML = homeHtml;
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
